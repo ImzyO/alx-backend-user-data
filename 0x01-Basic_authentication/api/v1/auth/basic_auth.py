592110d82b425f3db04a5c0a6c3f52b49ae20456
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-02-08 14:33:26 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,8 @@
 """Basic Auth implementation module
 """
 
-
-from api.v1.auth.auth import Auth
+from auth import Auth
+# from api.v1.auth.auth import Auth
 import base64
 from typing import TypeVar
 from models.user import User
