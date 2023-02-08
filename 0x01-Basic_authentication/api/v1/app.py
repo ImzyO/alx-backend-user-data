c6345d5895d9df55983a19a9686be75b83459cf8
Imani <100032916+ImzyO@users.noreply.github.com>
2023-02-08 17:09:00 +0300

    bug fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,7 +3,7 @@
 Route module for the API
 """
 from os import getenv
-from views.index import app_views
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
