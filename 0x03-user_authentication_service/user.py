303fc75bc96c1342b18a7e1a587920aaca0f90b9
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-02-26 16:40:38 +0000

    added whitespace

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,9 +14,11 @@ Base = declarative_base()
 
 class User(Base):
     """User class"""
-    
+   
+
     __tablename__ = 'users'
-    
+   
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
