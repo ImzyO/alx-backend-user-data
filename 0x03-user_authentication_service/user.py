776082695ce852e9a5d2738caef7d499886083ad
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-02-26 16:39:00 +0000

    changed

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -19,6 +19,6 @@ class User(Base):
     
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
