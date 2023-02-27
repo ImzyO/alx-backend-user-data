31e577e26866f1de3d70783433dd88ba43382a47
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-02-27 09:00:29 +0000

    added whitespaces

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-Session Authentication module
+Session Authentication module creates a session
 """
+
 
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -9,8 +10,9 @@ from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """SessionAuth class"""
+    """SessionAuth class"nherits from Auth"""
     user_id_by_session_id = {}
+
 
     def create_session(self, user_id: str = None) -> str:
         """this method creates a session ID for a user"""
@@ -20,17 +22,20 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """this method returns a User ID based on a Session ID"""
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
 
+
     def current_user(self, request=None):
         """this method returns a User instance based on cookie value"""
         _my_session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(_my_session_id)
         return User.get(user_id)
+
 
     def destroy_session(self, request=None):
         """this method deletes the user session - logout"""
