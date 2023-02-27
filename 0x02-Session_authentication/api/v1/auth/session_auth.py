db54f54413361e326fe0c4151061a1792047aaf2
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-02-27 09:07:04 +0000

    doc

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-"""
-Session Authentication module creates a session
-"""
-
+"""session authentication module"""
 
 from api.v1.auth.auth import Auth
 from models.user import User
