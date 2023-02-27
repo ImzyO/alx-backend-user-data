88573e985ed571f5940feae5b72a031fd651f58d
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-02-27 07:01:48 +0000

    added

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    """ abort with 401 error"""
+    abort(401)
