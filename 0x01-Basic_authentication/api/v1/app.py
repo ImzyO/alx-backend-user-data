c444e9e257bae09df8f4ca0f1e4cdb569d92d525
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-02-27 07:21:25 +0000

    request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,17 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+auth = None
+if getenv("AUTH_TYPE", None) == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+if getenv("AUTH_TYPE", None) == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler
@@ -33,6 +44,22 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.before_request
+def before_request() -> str:
+    """before request handler"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
