52a5831edd3f5307cd889434e94fb02ee641e9b7
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-02-27 08:28:36 +0000

    sessiion cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -30,3 +30,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """this method gets the current user"""
         None
+
+    def session_cookie(self, request=None):
+        """this function returns cookie value from a request
+        """
+        if request is None:
+            return None
+        _my_session_id = getenv("SESSION_NAME")
+        return request.cookies.get(_my_session_id)
