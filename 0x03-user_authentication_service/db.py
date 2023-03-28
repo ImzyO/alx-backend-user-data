20938c0b4374a83bf3f1205c67e5208a4b860970
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-03-28 16:46:54 +0000

    updat euser

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from user import Base, User
 
 from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+#from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import NoResultFound
 from typing import TypeVar
 
